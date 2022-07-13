fa4899ee6319606ca7054c7cacb80d05461a5a3a
Bayathy <misosoup0321@icloud.com>
2022-07-14 04:49:46 +0900

    remove: remove chakra-ui emotion babel-core

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { theme } from './theme/theme'
 
 ReactDOM.render(
-   <React.StrictMode>
-      <ChakraProvider theme={theme}></ChakraProvider>
-   </React.StrictMode>,
+   <React.StrictMode></React.StrictMode>,
    document.querySelector('#root')
 )
--- src/theme/theme.ts
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "@chakra-ui/react"
-
-export const theme = extendTheme({
-  colors: {
-    primary:'#0054a7',
-    secondry:'#aaabab',
-    extra:'f7bba2'
-  },
-})
