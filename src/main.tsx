d5f2bb9c98cfb560921d7b125e2dd51c7cb0f7a9
Bayathy <misosoup0321@icloud.com>
2022-07-14 05:49:49 +0900

    install:install twin.macro tailwind emotion

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { GlobalStyles } from 'twin.macro'
+import App from './App'
 
 ReactDOM.render(
-   <React.StrictMode></React.StrictMode>,
+   <React.StrictMode>
+      <GlobalStyles />
+      <App />
+   </React.StrictMode>,
    document.querySelector('#root')
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-macros',
+          [
+            '@emotion/babel-plugin-jsx-pragmatic',
+            {
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react',
+            },
+          ],
+          [
+            '@babel/plugin-transform-react-jsx',
+            { pragma: '__cssprop' },
+            'twin.macro',
+          ],
+        ],
+      },
+    }),
+  ],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
