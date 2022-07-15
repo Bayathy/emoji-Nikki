7ff72e87bc115d5675a25b2ec79a88a8cfada2c3
Bayathy <misosoup0321@icloud.com>
2022-07-15 21:33:51 +0900

    update: add theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors:{
+      'primary':'#fdd000',
+      'secondary':'#9fd9f6',
+      'extra':'0068b7'
+    },
     extend: {},
   },
   plugins: [],
