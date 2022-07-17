26020066c95fecd9d2d4d8fe91c3a71111932b11
Bayathy <misosoup0321@icloud.com>
2022-07-17 18:19:37 +0900

    add: create button

--- src/components/atom/Button/index.tsx
+++ src/components/atom/Button/index.tsx
@@ -1,9 +1,16 @@
+import { SerializedStyles } from '@emotion/react'
 import React from 'react'
+import tw from 'twin.macro'
 
 type Button = {
-   children: React.ReactNode
+   children?: React.ReactNode
+   css?: SerializedStyles
 } & React.ComponentProps<'button'>
 
-export const Button: React.FC<Button> = () => {
-   return <></>
+export const Button: React.FC<Button> = ({ children, ...rest }) => {
+   return (
+      <button {...rest} css={tw`bg-extra rounded-2xl px-8`}>
+         aaa
+      </button>
+   )
 }
