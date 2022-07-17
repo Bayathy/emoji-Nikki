b0b1242b3faa943ee47ccec5bfa9b9bdead0465b
Bayathy <misosoup0321@icloud.com>
2022-07-18 02:45:40 +0900

    remove: remove storybook

--- src/components/atom/Button/index.tsx
+++ src/components/atom/Button/index.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 import tw from 'twin.macro'
 
 type Button = {
-   children?: React.ReactNode
+   children?: string
    css?: SerializedStyles
 } & React.ComponentProps<'button'>
 
 export const Button: React.FC<Button> = ({ children, ...rest }) => {
    return (
-      <button {...rest} css={tw`bg-extra rounded-2xl px-8`}>
-         aaa
+      <button {...rest} css={tw`border-b-extra rounded-2xl px-8`}>
+         {children}
       </button>
    )
 }
