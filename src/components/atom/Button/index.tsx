301c511471b749a2bf5f9a9de00f8dbb33ae6e0d
Bayathy <misosoup0321@icloud.com>
2022-07-20 03:35:18 +0900

    fix: resoleve conflict

--- src/components/atom/Button/index.tsx
+++ src/components/atom/Button/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SerializedStyles } from '@emotion/react'
 import React from 'react'
 import tw from 'twin.macro'
@@ -14,21 +13,3 @@ export const Button: React.FC<Button> = ({ children, ...rest }) => {
       </button>
    )
 }
-=======
-import { SerializedStyles } from '@emotion/react'
-import React from 'react'
-import tw from 'twin.macro'
-
-type Button = {
-   children?: string
-   css?: SerializedStyles
-} & React.ComponentProps<'button'>
-
-export const Button: React.FC<Button> = ({ children, ...rest }) => {
-   return (
-      <button {...rest} css={tw`bg-extra rounded-2xl px-8`}>
-         {children}
-      </button>
-   )
-}
->>>>>>> b235f2c816d8b4468e70ac1c909628b36f3ab065
