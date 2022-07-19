20b534045691848b098224b4589d57498a4aa5d8
Bayathy <misosoup0321@icloud.com>
2022-07-20 03:01:57 +0900

    update: add props at Box

--- src/components/atom/Box/index.tsx
+++ src/components/atom/Box/index.tsx
@@ -5,13 +5,15 @@ type Box = {
    limeted?: boolean
 } & Component<'div'>
 
-export const Box: React.FC<Box> = ({ children, limeted }) => {
+export const Box: React.FC<Box> = ({ children, limeted, ...rest }) => {
    return (
       <>
          {limeted ? (
-            <div css={tw`max-w-5xl`}>{children}</div>
+            <div css={tw`max-w-5xl`} {...rest}>
+               {children}
+            </div>
          ) : (
-            <div>{children}</div>
+            <div {...rest}>{children}</div>
          )}
       </>
    )
