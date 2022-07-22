3b5552531ee7c34f203eea7ec30afdc081162171
Bayathy <misosoup0321@icloud.com>
2022-07-22 16:14:47 +0900

    update: style Header

--- src/components/molecular/Header/index.tsx
+++ src/components/molecular/Header/index.tsx
@@ -1,6 +1,7 @@
 import tw from 'twin.macro'
 import React from 'react'
 import { Box } from '../../atom/Box'
+import { Card } from '../../atom/Card'
 
 export const Header: React.FC = () => {
    return (
@@ -11,7 +12,17 @@ export const Header: React.FC = () => {
             h-16
          `}
       >
-         <Box limeted styles={tw`w-full`}></Box>
+         <Box limeted styles={tw`w-full h-full flex items-center`}>
+            <Card
+               styles={tw`
+               w-max
+               bg-white
+               ml-8
+            `}
+            >
+               <p css={tw`px-8`}>title</p>
+            </Card>
+         </Box>
       </Box>
    )
 }
